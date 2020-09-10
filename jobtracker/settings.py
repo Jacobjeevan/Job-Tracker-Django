d39296075ff23f71b92937c511a2ee2ea43944c4
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-10 15:48:31 -0500

    Django heroku
    - Disabled staticfiles option for django_heroku (allowing heroku to make use of S3 buckets)
    	- Works locally, so this should work online as well.

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -163,6 +163,4 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
