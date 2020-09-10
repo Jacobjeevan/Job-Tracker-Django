d70401e1858e87f3e9b7753c22298b2c6ff369ab
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-10 14:27:32 -0500

    removed staticfiles_dirs; testing with heroku

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -161,5 +161,3 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
